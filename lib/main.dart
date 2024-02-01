97e6722d881f121998bd6da5de692f9fe2797b2f
Abdul Hannan <abdulhannan8540680@gmail.com>
2024-02-01 10:07:58 +0500

    rotating animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3;
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '3D Cube',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -20,11 +24,127 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late AnimationController _xController;
+  late AnimationController _yController;
+  late AnimationController _zController;
+
+  late Tween<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _xController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 20),
+    );
+    _yController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 30),
+    );
+    _zController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 40),
+    );
+    _animation = Tween<double>(begin: 0, end: pi * 2);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _xController.dispose();
+    _yController.dispose();
+    _zController.dispose();
+  }
+
+  double size = 100;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    _xController
+      ..reset()
+      ..repeat();
+    _yController
+      ..reset()
+      ..repeat();
+    _zController
+      ..reset()
+      ..repeat();
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF294D4A),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size,
+            width: double.maxFinite,
+          ),
+          AnimatedBuilder(
+              animation: Listenable.merge([
+                _xController,
+                _yController,
+                _zController,
+              ]),
+              builder: (context, child) {
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..rotateX(_animation.evaluate(_xController))
+                    ..rotateY(_animation.evaluate(_yController))
+                    ..rotateZ(_animation.evaluate(_zController)),
+                  child: Stack(
+                    children: [
+                      // back
+                      Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()
+                          ..translate(
+                            Vector3(0, 0, -size),
+                          ),
+                        child: Container(
+                          width: size,
+                          height: size,
+                          color: Colors.purple,
+                        ),
+                      ),
+                      // left
+                      Transform(
+                        alignment: Alignment.centerLeft,
+                        transform: Matrix4.identity()..rotateY(pi / 2),
+                        child: Container(
+                          width: size,
+                          height: size,
+                          color: Colors.red,
+                        ),
+                      ),
+                      // right
+                      Transform(
+                        alignment: Alignment.centerRight,
+                        transform: Matrix4.identity()..rotateY(-pi / 2),
+                        child: Container(
+                          width: size,
+                          height: size,
+                          color: Colors.red,
+                        ),
+                      ),
+                      // front
+                      Container(
+                        width: size,
+                        height: size,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                );
+              })
+        ],
+      ),
+    );
   }
 }
